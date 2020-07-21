04c318bf9640dcb4a5a37e5ee9aef6fb1eb025da
Khushdeep <anmoldhir25@gmail.com>
2020-07-21 15:52:29 +1200

    Add project files.

---
+++ ElephantRace/RacingForm.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace ElephantRace
+{
+    partial class RacingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RacingForm));
+            this.Lion1 = new System.Windows.Forms.PictureBox();
+            this.Lion2 = new System.Windows.Forms.PictureBox();
+            this.Lion3 = new System.Windows.Forms.PictureBox();
+            this.Lion4 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.JoeBet = new System.Windows.Forms.Label();
+            this.JennyBet = new System.Windows.Forms.Label();
+            this.AlBet = new System.Windows.Forms.Label();
+            this.BetDetails = new System.Windows.Forms.Label();
+            this.race = new System.Windows.Forms.Button();
+            this.LionNumber = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BettingAmount = new System.Windows.Forms.NumericUpDown();
+            this.Bets = new System.Windows.Forms.Button();
+            this.JoeRButton = new System.Windows.Forms.RadioButton();
+            this.JennyRButton = new System.Windows.Forms.RadioButton();
+            this.AlButton = new System.Windows.Forms.RadioButton();
+            this.MaximumBet = new System.Windows.Forms.Label();
+            this.racetrack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Lion1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lion2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lion3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lion4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LionNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BettingAmount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racetrack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lion1
+            // 
+            this.Lion1.Image = ((System.Drawing.Image)(resources.GetObject("Lion1.Image")));
+            this.Lion1.Location = new System.Drawing.Point(13, 13);
+            this.Lion1.Margin = new System.Windows.Forms.Padding(4);
+            this.Lion1.Name = "Lion1";
+            this.Lion1.Size = new System.Drawing.Size(109, 88);
+            this.Lion1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Lion1.TabIndex = 1;
+            this.Lion1.TabStop = false;
+            // 
+            // Lion2
+            // 
+            this.Lion2.Image = ((System.Drawing.Image)(resources.GetObject("Lion2.Image")));
+            this.Lion2.Location = new System.Drawing.Point(17, 118);
+            this.Lion2.Margin = new System.Windows.Forms.Padding(4);
+            this.Lion2.Name = "Lion2";
+            this.Lion2.Size = new System.Drawing.Size(105, 87);
+            this.Lion2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Lion2.TabIndex = 2;
+            this.Lion2.TabStop = false;
+            // 
+            // Lion3
+            // 
+            this.Lion3.Image = ((System.Drawing.Image)(resources.GetObject("Lion3.Image")));
+            this.Lion3.Location = new System.Drawing.Point(17, 223);
+            this.Lion3.Margin = new System.Windows.Forms.Padding(4);
+            this.Lion3.Name = "Lion3";
+            this.Lion3.Size = new System.Drawing.Size(104, 85);
+            this.Lion3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Lion3.TabIndex = 3;
+            this.Lion3.TabStop = false;
+            // 
+            // Lion4
+            // 
+            this.Lion4.Image = ((System.Drawing.Image)(resources.GetObject("Lion4.Image")));
+            this.Lion4.Location = new System.Drawing.Point(18, 326);
+            this.Lion4.Margin = new System.Windows.Forms.Padding(4);
+            this.Lion4.Name = "Lion4";
+            this.Lion4.Size = new System.Drawing.Size(104, 84);
+            this.Lion4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Lion4.TabIndex = 4;
+            this.Lion4.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox1.Controls.Add(this.JoeBet);
+            this.groupBox1.Controls.Add(this.JennyBet);
+            this.groupBox1.Controls.Add(this.AlBet);
+            this.groupBox1.Controls.Add(this.BetDetails);
+            this.groupBox1.Controls.Add(this.race);
+            this.groupBox1.Controls.Add(this.LionNumber);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.BettingAmount);
+            this.groupBox1.Controls.Add(this.Bets);
+            this.groupBox1.Controls.Add(this.JoeRButton);
+            this.groupBox1.Controls.Add(this.JennyRButton);
+            this.groupBox1.Controls.Add(this.AlButton);
+            this.groupBox1.Controls.Add(this.MaximumBet);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 447);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1069, 215);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Elephant Race Betting System";
+            // 
+            // JoeBet
+            // 
+            this.JoeBet.BackColor = System.Drawing.SystemColors.Info;
+            this.JoeBet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.JoeBet.Location = new System.Drawing.Point(649, 110);
+            this.JoeBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.JoeBet.Name = "JoeBet";
+            this.JoeBet.Size = new System.Drawing.Size(386, 20);
+            this.JoeBet.TabIndex = 13;
+            // 
+            // JennyBet
+            // 
+            this.JennyBet.BackColor = System.Drawing.SystemColors.Info;
+            this.JennyBet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.JennyBet.Location = new System.Drawing.Point(649, 80);
+            this.JennyBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.JennyBet.Name = "JennyBet";
+            this.JennyBet.Size = new System.Drawing.Size(386, 20);
+            this.JennyBet.TabIndex = 12;
+            // 
+            // AlBet
+            // 
+            this.AlBet.BackColor = System.Drawing.SystemColors.Info;
+            this.AlBet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AlBet.Location = new System.Drawing.Point(649, 50);
+            this.AlBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AlBet.Name = "AlBet";
+            this.AlBet.Size = new System.Drawing.Size(386, 20);
+            this.AlBet.TabIndex = 11;
+            // 
+            // BetDetails
+            // 
+            this.BetDetails.AutoSize = true;
+            this.BetDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BetDetails.Location = new System.Drawing.Point(645, 20);
+            this.BetDetails.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BetDetails.Name = "BetDetails";
+            this.BetDetails.Size = new System.Drawing.Size(48, 20);
+            this.BetDetails.TabIndex = 10;
+            this.BetDetails.Text = "Bets";
+            // 
+            // race
+            // 
+            this.race.BackColor = System.Drawing.Color.Gray;
+            this.race.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.race.Location = new System.Drawing.Point(856, 145);
+            this.race.Margin = new System.Windows.Forms.Padding(4);
+            this.race.Name = "race";
+            this.race.Size = new System.Drawing.Size(180, 37);
+            this.race.TabIndex = 9;
+            this.race.Text = "START RACE";
+            this.race.UseVisualStyleBackColor = false;
+            this.race.Click += new System.EventHandler(this.race_Click);
+            // 
+            // LionNumber
+            // 
+            this.LionNumber.Location = new System.Drawing.Point(529, 106);
+            this.LionNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.LionNumber.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.LionNumber.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.LionNumber.Name = "LionNumber";
+            this.LionNumber.Size = new System.Drawing.Size(85, 22);
+            this.LionNumber.TabIndex = 8;
+            this.LionNumber.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Elephant number";
+            // 
+            // BettingAmount
+            // 
+            this.BettingAmount.Location = new System.Drawing.Point(531, 47);
+            this.BettingAmount.Margin = new System.Windows.Forms.Padding(4);
+            this.BettingAmount.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.BettingAmount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.BettingAmount.Name = "BettingAmount";
+            this.BettingAmount.Size = new System.Drawing.Size(84, 22);
+            this.BettingAmount.TabIndex = 6;
+            this.BettingAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Bets
+            // 
+            this.Bets.BackColor = System.Drawing.Color.Gray;
+            this.Bets.Location = new System.Drawing.Point(364, 37);
+            this.Bets.Margin = new System.Windows.Forms.Padding(4);
+            this.Bets.Name = "Bets";
+            this.Bets.Size = new System.Drawing.Size(112, 44);
+            this.Bets.TabIndex = 5;
+            this.Bets.Text = "Place Bet";
+            this.Bets.UseVisualStyleBackColor = false;
+            this.Bets.Click += new System.EventHandler(this.Bets_Click);
+            // 
+            // JoeRButton
+            // 
+            this.JoeRButton.AutoSize = true;
+            this.JoeRButton.Checked = true;
+            this.JoeRButton.Location = new System.Drawing.Point(19, 119);
+            this.JoeRButton.Margin = new System.Windows.Forms.Padding(4);
+            this.JoeRButton.Name = "JoeRButton";
+            this.JoeRButton.Size = new System.Drawing.Size(52, 21);
+            this.JoeRButton.TabIndex = 3;
+            this.JoeRButton.TabStop = true;
+            this.JoeRButton.Text = "Joe";
+            this.JoeRButton.UseVisualStyleBackColor = true;
+            this.JoeRButton.CheckedChanged += new System.EventHandler(this.JoeButton_CheckedChanged);
+            // 
+            // JennyRButton
+            // 
+            this.JennyRButton.AutoSize = true;
+            this.JennyRButton.Location = new System.Drawing.Point(19, 90);
+            this.JennyRButton.Margin = new System.Windows.Forms.Padding(4);
+            this.JennyRButton.Name = "JennyRButton";
+            this.JennyRButton.Size = new System.Drawing.Size(67, 21);
+            this.JennyRButton.TabIndex = 2;
+            this.JennyRButton.Text = "Jenny";
+            this.JennyRButton.UseVisualStyleBackColor = true;
+            this.JennyRButton.CheckedChanged += new System.EventHandler(this.JennyButton_CheckedChanged);
+            // 
+            // AlButton
+            // 
+            this.AlButton.AutoSize = true;
+            this.AlButton.Location = new System.Drawing.Point(19, 60);
+            this.AlButton.Margin = new System.Windows.Forms.Padding(4);
+            this.AlButton.Name = "AlButton";
+            this.AlButton.Size = new System.Drawing.Size(41, 21);
+            this.AlButton.TabIndex = 1;
+            this.AlButton.Text = "Al";
+            this.AlButton.UseVisualStyleBackColor = true;
+            this.AlButton.CheckedChanged += new System.EventHandler(this.AlButton_CheckedChanged);
+            // 
+            // MaximumBet
+            // 
+            this.MaximumBet.AutoSize = true;
+            this.MaximumBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximumBet.Location = new System.Drawing.Point(15, 30);
+            this.MaximumBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MaximumBet.Name = "MaximumBet";
+            this.MaximumBet.Size = new System.Drawing.Size(120, 20);
+            this.MaximumBet.TabIndex = 0;
+            this.MaximumBet.Text = "Maximum bet";
+            // 
+            // racetrack
+            // 
+            this.racetrack.BackColor = System.Drawing.SystemColors.Control;
+            this.racetrack.Image = ((System.Drawing.Image)(resources.GetObject("racetrack.Image")));
+            this.racetrack.Location = new System.Drawing.Point(-1, 4);
+            this.racetrack.Margin = new System.Windows.Forms.Padding(4);
+            this.racetrack.Name = "racetrack";
+            this.racetrack.Size = new System.Drawing.Size(1069, 416);
+            this.racetrack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.racetrack.TabIndex = 0;
+            this.racetrack.TabStop = false;
+            // 
+            // RacingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 661);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Lion4);
+            this.Controls.Add(this.Lion3);
+            this.Controls.Add(this.Lion2);
+            this.Controls.Add(this.Lion1);
+            this.Controls.Add(this.racetrack);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RacingForm";
+            this.Text = "Race Course";
+            ((System.ComponentModel.ISupportInitialize)(this.Lion1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lion2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lion3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lion4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LionNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BettingAmount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racetrack)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Lion1;
+        private System.Windows.Forms.PictureBox Lion2;
+        private System.Windows.Forms.PictureBox Lion3;
+        private System.Windows.Forms.PictureBox Lion4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton JoeRButton;
+        private System.Windows.Forms.RadioButton JennyRButton;
+        private System.Windows.Forms.RadioButton AlButton;
+        private System.Windows.Forms.Label MaximumBet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown BettingAmount;
+        private System.Windows.Forms.Button Bets;
+        private System.Windows.Forms.NumericUpDown LionNumber;
+        private System.Windows.Forms.Button race;
+        private System.Windows.Forms.Label BetDetails;
+        private System.Windows.Forms.Label AlBet;
+        private System.Windows.Forms.Label JoeBet;
+        private System.Windows.Forms.Label JennyBet;
+        private System.Windows.Forms.PictureBox racetrack;
+    }
+}
---
+++ ElephantRace/RacingForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ElephantRace
+{
+    public partial class RacingForm : Form
+    {
+        Elephant[] Lions = new Elephant[4];
+
+        Factory pFactory = new Factory();
+        Punter[] punters = new Punter[3];
+
+        public RacingForm()
+        {
+            InitializeComponent();
+            SetupRaceTrack();
+        }
+
+        private void SetupRaceTrack()
+        {
+
+            Elephant.StartingPosition1 = Lion1.Right - racetrack.Left;
+            Elephant.RacetrackLength1 = racetrack.Size.Width - 70; //fixing length of race - till finish line
+
+            Lions[0] = new Elephant() { LionPictureBox = Lion1 };
+            Lions[1] = new Elephant() { LionPictureBox = Lion2 };
+            Lions[2] = new Elephant() { LionPictureBox = Lion3 };
+            Lions[3] = new Elephant() { LionPictureBox = Lion4 };
+
+            punters[0] = pFactory.getPunter("Al", MaximumBet, AlBet); //getting Al object from factory class
+            punters[1] = pFactory.getPunter("Jenny", MaximumBet, JennyBet); //getting Jenny object from factory class
+            punters[2] = pFactory.getPunter("Joe", MaximumBet, JoeBet); //getting Joe object from factory class
+
+
+            foreach (Punter punter in punters)
+            {
+                punter.UpdateLabels();
+            }
+        }
+
+        private void AlButton_CheckedChanged(object sender, EventArgs e)
+        {
+            setMaximumBetTextLabel(punters[0].Cash);
+        }
+
+        private void JennyButton_CheckedChanged(object sender, EventArgs e)
+        {
+            setMaximumBetTextLabel(punters[1].Cash);
+        }
+
+        private void JoeButton_CheckedChanged(object sender, EventArgs e)
+        {
+            setMaximumBetTextLabel(punters[2].Cash);
+        }
+
+        private void setMaximumBetTextLabel(int Cash)
+        {
+            MaximumBet.Text = String.Format("Maximum Bet: ${0}", Cash);
+        }
+
+        // setting the bet for each Punter and updating labels respectively
+        private void Bets_Click(object sender, EventArgs e)
+        {
+            int PunterNum = 0;
+
+            if (AlButton.Checked)
+            {
+                PunterNum = 0;
+            }
+            if (JennyRButton.Checked)
+            {
+                PunterNum = 1;
+            }
+            if (JoeRButton.Checked)
+            {
+                PunterNum = 2;
+            }
+
+            punters[PunterNum].PlaceBet((int)BettingAmount.Value, (int)LionNumber.Value);
+            punters[PunterNum].UpdateLabels();
+        }
+
+        private void race_Click(object sender, EventArgs e)
+        {
+            bool NoWinner = true;
+            int winningLion;
+            race.Enabled = false; //disable start race button
+
+            while (NoWinner)
+            { // loop until we have a winner
+                Application.DoEvents();
+                for (int i = 0; i < Lions.Length; i++)
+                {
+                    if (Elephant.Run(Lions[i]))
+                    {
+                        winningLion = i + 1;
+                        NoWinner = false;
+                        MessageBox.Show("We have a winner - Lion #" + winningLion);
+                        foreach (Punter punter in punters)
+                        {
+                            if (punter.bet != null)
+                            {
+                                punter.Collect(winningLion); //give double amount to all who've won or deduce betted amount
+                                punter.bet = null;
+                                punter.UpdateLabels();
+                            }
+                        }
+                        foreach (Elephant Lion in Lions)
+                        {
+                            Lion.TakeStartingPosition();
+                        }
+                        break;
+                    }
+                }
+            }
+            if (punters[0].busted && punters[1].busted && punters[2].busted)
+            {  //stop punters from betting if they run out of cash
+                String message = "Do you want to Play Again?";
+                String title = "GAME OVER!";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show(message, title, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    SetupRaceTrack(); //restart game
+                }
+                else
+                {
+                    this.Close();
+                }
+
+            }
+            race.Enabled = true; //enable race button 
+        }
+
+    }
+}
